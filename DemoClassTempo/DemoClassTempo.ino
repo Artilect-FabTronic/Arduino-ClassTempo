a5bb347e4427e247c36220bfc8d54f426dca3c9d
arnaulddev <biganzoli@laplace.univ-tlse.fr>
2020-11-05 19:09:56 +0100

    Creation du projet Arduino

---
+++ DemoClassTempo/DemoClassTempo.ino
@@ -0,0 +1,15 @@
+/*
+ * Demo de la création d'une classe en C++
+ * TODO: créer la classe Tempo
+ * voir les fichiers a venir *.ino, *.cpp, *.h
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Arduino-ClassTempo
+
 Demo de la créarion d'une classe Tempo
+
+TODO:
+
+- [ ] créer la classe Tempo
