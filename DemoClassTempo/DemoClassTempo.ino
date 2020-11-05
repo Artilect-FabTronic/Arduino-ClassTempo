64245fe29e4889f4363f7bb857f5c20b6e2f55f5
arnaulddev <biganzoli@laplace.univ-tlse.fr>
2020-11-05 19:20:26 +0100

    Add new files

--- DemoClassTempo/DemoClassTempo.ino
+++ DemoClassTempo/DemoClassTempo.ino
@@ -1,15 +1,23 @@
 /*
- * Demo de la création d'une classe en C++
- * TODO: créer la classe Tempo
- * voir les fichiers a venir *.ino, *.cpp, *.h
- */
+   Demo de la création d'une classe en C++
+   TODO: créer la classe Tempo
+   voir les fichiers a venir *.ino, *.cpp, *.h
+*/
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  acquisitionADC(A0);
+  delay(10);        // delay in between two acquire
+}
 
+void acquisitionADC(int analog_pin) {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(analog_pin);
+  // print out the value you read:
+  Serial.println(sensorValue);
 }
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Demo de la créarion d'une classe Tempo
 
 TODO:
 
+- [x] créer les fichiers sources
 - [ ] créer la classe Tempo
